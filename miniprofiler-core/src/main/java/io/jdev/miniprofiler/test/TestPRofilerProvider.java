4f76e063877acb9c88ea240b765e482b6faef26b
Tom Dunstan <github@tomd.cc>
2013-09-19 10:05:13 +0930

    Profiler provider implementation to help with testing.
    May be useful for consumers of the library, so it's going in the main source
    rather than with the tests.

---
+++ miniprofiler-core/src/main/java/io/jdev/miniprofiler/test/TestPRofilerProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jdev.miniprofiler.test;
+
+import io.jdev.miniprofiler.*;
+import io.jdev.miniprofiler.storage.Storage;
+
+/**
+ * Profiler provider to assist testing. Has a single profiler instance and performs
+ * no storage.
+ */
+
+public class TestProfilerProvider extends BaseProfilerProvider {
+
+	private Profiler profiler;
+
+	@Override
+	protected void profilerCreated(Profiler profiler) {
+		this.profiler = profiler;
+	}
+
+	@Override
+	protected void profilerStopped(Profiler profiler) {
+	}
+
+	@Override
+	protected Profiler lookupCurrentProfiler() {
+		return profiler;
+	}
+}
