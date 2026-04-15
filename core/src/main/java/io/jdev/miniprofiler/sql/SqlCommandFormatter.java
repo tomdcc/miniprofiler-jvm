e5533f79511ccfd876e5791dd622df9acaf2a030
Tom Dunstan <github@tomd.cc>
2026-04-15 16:02:45 +0930

    Make SqlCommandFormatter configurable via miniprofiler properties
    Construct a dialect-specific SqlFormatter and FormatConfig at
    construction time, populated via the existing miniprofiler config
    mechanism (system properties prefixed miniprofiler. plus
    /miniprofiler.properties on the classpath, following the pattern in
    ProfilerUiConfig). Recognised keys under the sql.format. prefix:
    
      sql.format.dialect               (default StandardSql)
      sql.format.indent.width          (default 2)
      sql.format.uppercase             (default false)
      sql.format.lines.between.queries (default 1)
      sql.format.max.column.length     (default 50)
    
    A package-private constructor accepting Properties allows tests to
    exercise specific configurations without mutating system state.

--- core/src/main/java/io/jdev/miniprofiler/sql/SqlCommandFormatter.java
+++ core/src/main/java/io/jdev/miniprofiler/sql/SqlCommandFormatter.java
@@ -17,21 +17,77 @@
 package io.jdev.miniprofiler.sql;
 
 import com.github.vertical_blank.sqlformatter.SqlFormatter;
+import com.github.vertical_blank.sqlformatter.core.FormatConfig;
+import com.github.vertical_blank.sqlformatter.languages.Dialect;
 import io.jdev.miniprofiler.format.CommandFormatter;
+import io.jdev.miniprofiler.internal.ConfigHelper.PropertiesWithPrefix;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static io.jdev.miniprofiler.internal.ConfigHelper.getProperty;
+import static io.jdev.miniprofiler.internal.ConfigHelper.loadPropertiesFile;
 
 /**
  * A {@link CommandFormatter} for SQL commands. Delegates formatting to
  * {@link SqlFormatter}.
+ *
+ * <p>Configuration is read from system properties (with prefix {@code miniprofiler.})
+ * and {@code /miniprofiler.properties} on the classpath. Recognised keys (without
+ * the system-property prefix) are:</p>
+ * <ul>
+ *   <li>{@code sql.format.dialect} &mdash; name of a {@link Dialect}, default {@code StandardSql}</li>
+ *   <li>{@code sql.format.indent.width} &mdash; integer number of spaces per indent level, default {@code 2}</li>
+ *   <li>{@code sql.format.uppercase} &mdash; boolean, uppercase keywords, default {@code false}</li>
+ *   <li>{@code sql.format.lines.between.queries} &mdash; integer, default {@code 1}</li>
+ *   <li>{@code sql.format.max.column.length} &mdash; integer, default {@code 50}</li>
+ * </ul>
  */
 public class SqlCommandFormatter implements CommandFormatter {
 
-    /** Creates a new instance. */
+    private static final String SYSTEM_PROP_PREFIX = "miniprofiler.";
+    private static final String MINIPROFILER_RESOURCE_NAME = "/miniprofiler.properties";
+
+    private final SqlFormatter.Formatter formatter;
+    private final FormatConfig formatConfig;
+
+    /**
+     * Creates a new instance, loading configuration from system properties and
+     * {@code /miniprofiler.properties} on the classpath.
+     */
     public SqlCommandFormatter() {
+        this(System.getProperties(), loadPropertiesFile(MINIPROFILER_RESOURCE_NAME));
+    }
+
+    SqlCommandFormatter(Properties systemProps, Properties propsFileProps) {
+        // system props >> miniprofiler.properties >> defaults
+        List<PropertiesWithPrefix> propsList = new ArrayList<PropertiesWithPrefix>(2);
+        propsList.add(new PropertiesWithPrefix(systemProps, SYSTEM_PROP_PREFIX));
+        if (propsFileProps != null) {
+            propsList.add(new PropertiesWithPrefix(propsFileProps, ""));
+        }
+
+        Dialect dialect = getProperty(propsList, "sql.format.dialect", Dialect.class, Dialect.StandardSql);
+        this.formatter = SqlFormatter.of(dialect);
+        int indentWidth = getProperty(propsList, "sql.format.indent.width", Integer.valueOf(2));
+        this.formatConfig = FormatConfig.builder()
+            .indent(spaces(indentWidth))
+            .uppercase(getProperty(propsList, "sql.format.uppercase", false))
+            .linesBetweenQueries(getProperty(propsList, "sql.format.lines.between.queries", Integer.valueOf(1)))
+            .maxColumnLength(getProperty(propsList, "sql.format.max.column.length", Integer.valueOf(FormatConfig.DEFAULT_COLUMN_MAX_LENGTH)))
+            .build();
+    }
+
+    private static String spaces(int n) {
+        char[] chars = new char[n];
+        java.util.Arrays.fill(chars, ' ');
+        return new String(chars);
     }
 
     @Override
     public String format(String command) {
-        return SqlFormatter.format(command);
+        return formatter.format(command, formatConfig);
     }
 
     @Override
